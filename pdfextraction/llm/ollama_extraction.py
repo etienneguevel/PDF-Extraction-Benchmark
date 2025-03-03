cc6a1c5fa32bbaf22a7546d56a0ca85664677b59
Etienne Guevel <etienneguev@gmail.com>
2025-03-03 19:27:12 +0100

    feat: add the cover page in main_parts prompt

--- pdfextraction/llm/ollama_extraction.py
+++ pdfextraction/llm/ollama_extraction.py
@@ -54,14 +54,23 @@ def main_parts_prompt(text: str) -> str:
     # extract the main sections from the pdf
     pattern_parts = r"(\*\*\d+\.(?P<title>[^*]+)\*\*[\s\S]+?)(?=\n\*\*|$)"
     sections = re.findall(pattern_parts, text)
-    main_parts = ["results", "intro", "conclusion"]
-    txt = "\n".join([t for t, title in sections if any([m in title.lower() for m in main_parts])])
+
+    # find the intro section and the cover page coming before
+    title_intro = [title for _, title in sections if "intro" in title.lower()][0]
+    cover_page =  text[:text.find(title_intro)]
+
+    # find the conclusion and results part as well
+    txt = "\n".join([
+        t for t, title in sections if any(
+            [m in title.lower() for m in ["results", "conclusion", "intro"]]
+        )
+    ])
 
     prompt = f"""
     You are given parts from a scientific paper, you are tasked with
     extracting information from these parts.
     Here is the text:
-    {txt}
+    {cover_page + txt}
     """
     
     return prompt
