e61b7b0a9f0017e0b512dcfcedae4647b677227d
Etienne Guevel <etienneguev@gmail.com>
2025-03-03 18:52:43 +0100

    feat: split text into sections

--- pdfextraction/llm/ollama_extraction.py
+++ pdfextraction/llm/ollama_extraction.py
@@ -1,9 +1,10 @@
 import argparse
 
 from ollama import chat
+from pydantic import BaseModel
 
 import pdfextraction.taxonomy.wsl_taxonomy as taxonomy
-
+from pdfextraction.llm.utils import open_file, ollama_available
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Extract OLLAMA from a paper")
@@ -16,23 +17,22 @@ def parse_args():
         help="Taxonomy to use for extraction"
     )
     parser.add_argument(
-        "--output-path",
+        "--output-name",
         type=str,
         default=None,
-        help="Path to save the extracted OLLAMA"
+        help="Name of the json where the extracted information will be stored"
     )
 
     return parser.parse_args()
 
-def open_file(text_path):
-    with open(text_path, "r") as f:
-        txt = f.read()
+
+def extract_ollama_from_paper(
+    text: str, model: str, tax: BaseModel
+) -> BaseModel:
     
-    return txt
+    if not ollama_available(model):
+        raise AttributeError(f"{model} is not among the local ollama models.")
 
-def extract_ollama_from_paper(text_path, model, tax):
-
-    text = open_file(text_path)
     prompt = f"""
     You are given a scientific paper. The first page corresponds to the where
     the title, authors, and abstract are located. The rest of the paper is
@@ -60,6 +60,8 @@ def extract_ollama_from_paper(text_path, model, tax):
 
 def main():
     args = parse_args()
+
+    # check if the given taxonomy name is within the coded taxonomies
     if args.taxonomy:
         callables = {
             name: obj for name, obj in taxonomy.__dict__.items() if callable(obj)
@@ -69,15 +71,20 @@ def main():
 
         except KeyError:
             raise ValueError(f"Taxonomy {args.taxonomy} not found")
-        
+
+    # if no given taxonomies, use the default one   
     else:
         tax = taxonomy.Paper
 
-    paper = extract_ollama_from_paper(args.text_path, args.model, tax)
+    # open and process the text
+    txt = open_file(args.text_path)
+    paper = extract_ollama_from_paper(txt, args.model, tax)
     
+    # print or save the output
     if not args.output_path:
         print(paper)
     
+    # save the result with the given name
     else:
         output_path = args.output_path.split(".")[0] + ".json"
         with open(output_path, "w") as f:
